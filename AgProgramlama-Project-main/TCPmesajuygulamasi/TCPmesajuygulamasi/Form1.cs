cb8573dc3e1b346974d7ed0a947454c63d047eae
Mert Çalışkan <mertcaliskan819@hotmail.com>
2025-07-18 16:22:26 +0300

    commit1

---
+++ AgProgramlama-Project-main/TCPmesajuygulamasi/TCPmesajuygulamasi/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+
+namespace TCPmesajuygulamasi
+{
+    public partial class Form1 : Form
+    {
+        string kullanici_id;
+        SqlConnection connection = new SqlConnection("server=localhost;database=agprogramlama;integrated security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+        private string GetLocalIPAddress()
+        {
+            string ipAddress = "";
+            foreach (var ip in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                // IPv4 adresini alıyoruz.
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip.ToString();
+                    break;
+                }
+            }
+            return ipAddress;
+        }
+       
+
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Kullanici Adi Veya Şifre Boş Bırakılamaz!!", "Boş Değer Hatası", MessageBoxButtons.OK);
+                return;
+            }
+            string sorgu = "select*from users where username=@username AND password=@pass";
+            SqlDataReader dr;
+            SqlCommand komut = new SqlCommand(sorgu, connection);
+            komut.Parameters.AddWithValue("@username", textBox1.Text);
+            komut.Parameters.AddWithValue("@pass", HashPassword(textBox2.Text));
+            connection.Open();
+            dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                string hosgeldinMesaj = "Hoşgeldin " + dr.GetString(1).ToUpper() + "\nİyi Çalışmalar"; kullanici_id = dr.GetByte(0).ToString();
+                string ip = GetLocalIPAddress();
+                MessageBox.Show(hosgeldinMesaj, "Hoşgeldin!");
+                sorgu = "UPDATE users set isOnline = @isOnline, ipAddress=@ip WHERE id ="+dr.GetByte(0).ToString();
+                connection.Close();
+                komut = new SqlCommand(sorgu, connection);
+                komut.Parameters.AddWithValue("@isOnline", 1);
+                komut.Parameters.AddWithValue("@ip", ip);
+                connection.Open();
+                komut.ExecuteNonQuery();
+                connection.Close();
+                mainScreen main = new mainScreen(kullanici_id);
+                main.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı Veya Şifre Yanlış!!", "Yanlışlık Var!!");
+            }
+            connection.Close();
+        }
+
+        private void createAcoount_Click(object sender, EventArgs e)
+        {
+            regs regform = new regs();
+            regform.Show();
+            this.Hide();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if(ID != "0")
+            //{
+            //    sorgu = "UPDATE users set isOnline = @isOnline WHERE id =" + dr.GetByte(0).ToString();
+            //    connection.Close();
+            //    komut = new SqlCommand(sorgu, connection);
+            //    komut.Parameters.AddWithValue("@isOnline", 1);
+            //    connection.Open();
+            //    komut.ExecuteNonQuery();
+            //    connection.Close();
+            //}
+            Application.Exit();
+        }
+    }
+}
---
+++ AgProgramlama-Project-main/TCPmesajuygulamasi/TCPmesajuygulamasi/mainScreen.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TCPmesajuygulamasi
+{
+    partial class mainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tCPİleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.portGüncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tCPİleToolStripMenuItem,
+            this.portGüncelleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(969, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tCPİleToolStripMenuItem
+            // 
+            this.tCPİleToolStripMenuItem.Name = "tCPİleToolStripMenuItem";
+            this.tCPİleToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.tCPİleToolStripMenuItem.Text = "TCP ile";
+            this.tCPİleToolStripMenuItem.Click += new System.EventHandler(this.tCPİleToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(969, 549);
+            this.panel1.TabIndex = 2;
+            // 
+            // portGüncelleToolStripMenuItem
+            // 
+            this.portGüncelleToolStripMenuItem.Name = "portGüncelleToolStripMenuItem";
+            this.portGüncelleToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.portGüncelleToolStripMenuItem.Text = "Port Güncelle";
+            this.portGüncelleToolStripMenuItem.Click += new System.EventHandler(this.portGüncelleToolStripMenuItem_Click);
+            // 
+            // mainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 579);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mainScreen";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainScreen_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tCPİleToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem portGüncelleToolStripMenuItem;
+    }
+}
